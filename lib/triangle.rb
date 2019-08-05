22618c8e3f3e98360f5a2a5110dbf6414f01ba89
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-05 20:36:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,8 +22,8 @@ class Triangle
   def validate_triangle
     real_triangle =  [(a + b > c),(c+b > a), (c+a > b) ]
     [a,b,c].each do |side|
-     real_triangle << false if side = 0  
-       raise TriangleError if real_triangle = false
+     real_triangle << false if side <= 0  
+       raise TriangleError if real_triangle = fals
      end 
     end 
   end 
