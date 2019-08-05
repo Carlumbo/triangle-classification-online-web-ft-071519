58f9b01b43cf20649d3c6d45c137c1ef229d1641
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-05 20:40:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,8 +24,7 @@ class Triangle
     [a,b,c].each do |side|
      real_triangle << false if side <= 0  
        raise TriangleError if real_triangle,include?(false)
-    end 
-   end 
+    
   
      class TriangleError < StandardError 
      end 
